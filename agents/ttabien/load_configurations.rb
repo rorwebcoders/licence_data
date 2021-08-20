659031f0af49cb837a4d56922a5e7fc1be8152b7
SudharshanaBabu <sudharshana.b@rorwebcoders.com>
2021-08-20 14:36:56 +0530

    added Attabien, Booktabien, Thetabienvip, Tabienmotorcycle & Ttabien

--- agents/ttabien/load_configurations.rb
+++ agents/ttabien/load_configurations.rb
@@ -15,7 +15,7 @@ require 'simple_xlsx_reader'
 ActiveRecord::Base.default_timezone = :utc
 require File.expand_path('../../lib/config/database_connection', __FILE__)
 puts require File.expand_path('../../../config/application', __FILE__)
-require File.expand_path('../../lib/models/tabiendd_detail', __FILE__)
+require File.expand_path('../../lib/models/ttabien_detail', __FILE__)
 #~ puts require File.expand_path('../../../config/boot', __FILE__)
 #~ require File.expand_path('../../lib/config/*
 
