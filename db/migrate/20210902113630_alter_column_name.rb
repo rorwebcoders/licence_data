4eb76c08632669aeb658b90f743c029c8c1713aa
rails-udhaya <sudharshana.b@rorwebcoders.com>
2021-09-02 17:12:25 +0530

    added index

---
+++ db/migrate/20210902113630_alter_column_name.rb
@@ -0,0 +1,46 @@
+class AlterColumnName < ActiveRecord::Migration[5.2]
+  def change
+  	change_column :tabien9_details, :license_number, :string
+		change_column :tabieninfinity_details, :license_number, :string
+		change_column :teeneetabien_details, :license_number, :string
+		change_column :kaitabien_details, :license_number, :string
+		change_column :tabien999_details, :license_number, :string
+		change_column :tabiend789_details, :license_number, :string
+		change_column :tabienrodnamchock_details, :license_number, :string
+		change_column :lekpramool_details, :license_number, :string
+		change_column :raktabien_details, :license_number, :string
+		change_column :markettabien_details, :license_number, :string
+		change_column :tabienrodvip_details, :license_number, :string
+		change_column :tabienmotorcycle_details, :license_number, :string
+		change_column :tabienchill_details, :license_number, :string
+		change_column :tabienrodpramool_details, :license_number, :string
+		change_column :buddytabien_details, :license_number, :string
+		change_column :tabienhot_details, :license_number, :string
+		change_column :booktabien_details, :license_number, :string
+		change_column :ttabien_details, :license_number, :string
+		change_column :thetabienvip_details, :license_number, :string
+		change_column :attabien_details, :license_number, :string
+		change_column :tabean_details, :license_number, :string
+		change_column :miracletabien_details, :license_number, :string
+		change_column :tabienmongkol_details, :license_number, :string
+		change_column :thabeiynthuk_details, :license_number, :string
+		change_column :tartabiensuay_details, :license_number, :string
+		change_column :tabiended_details, :license_number, :string
+		change_column :newtabien_details, :license_number, :string
+		change_column :tabien_details, :license_number, :string
+		change_column :tabiendd_details, :license_number, :string
+		change_column :bentabien_details, :license_number, :string
+		change_column :lekvip_details, :license_number, :string
+		change_column :thetabienrod_details, :license_number, :string
+		change_column :tabiensoey_details, :license_number, :string
+		change_column :tabiengraphic_details, :license_number, :string
+		change_column :forcesilltabien_details, :license_number, :string
+		change_column :clubtabien_details, :license_number, :string
+		change_column :bbsmartcar_details, :license_number, :string
+		change_column :tabienheng_details, :license_number, :string
+		change_column :tabienhiend_details, :license_number, :string
+		change_column :tabienrodthai_details, :license_number, :string
+		change_column :supertabien_details, :license_number, :string
+		change_column :selltabien_details, :license_number, :string
+  end
+end
