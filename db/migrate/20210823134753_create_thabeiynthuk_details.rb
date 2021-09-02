5c4583c57281d0ce539ea915321424c8f3a82918
rails-udhaya <sudharshana.b@rorwebcoders.com>
2021-09-02 12:06:54 +0530

    fixed the issue in table name

--- db/migrate/20210823134753_create_thabeiynthuk_details.rb
+++ db/migrate/20210823134753_create_thabeiynthuk_details.rb
@@ -1,7 +1,17 @@
 class CreateThabeiynthukDetails < ActiveRecord::Migration[5.2]
   def change
     create_table :thabeiynthuk_details do |t|
-
+    	t.text :url
+    	t.date :date_created
+    	t.text :license_group
+    	t.text :license_number
+    	t.text :price
+    	t.text :location
+    	t.text :license_status
+    	t.text :color
+    	t.text :processing_status
+    	t.text :is_duplicate
+    	t.text :price_status
       t.timestamps
     end
   end
